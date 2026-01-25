0dd01b3f40070f9de0af8546d391d97c56c9af3b
Ege Uysal <hi@egeuysal.com>
2026-01-25 08:51:25 -0600

    chore(lint): lint all files and fix errors

--- Javascript/course5editor.mjs
+++ Javascript/course5editor.mjs
@@ -1,15 +1,15 @@
-import { EditorView, basicSetup } from "codemirror"
-import { python } from "@codemirror/lang-python"
-import { oneDarkTheme } from "@codemirror/theme-one-dark"
+import { EditorView, basicSetup } from 'codemirror';
+import { python } from '@codemirror/lang-python';
+import { oneDarkTheme } from '@codemirror/theme-one-dark';
 
 export let editor1 = new EditorView({
-    doc: "\n\n\n\n",
-    extensions: [basicSetup, python(), oneDarkTheme],
-    parent: document.getElementsByClassName("editor1").item(0)
-})
+  doc: '\n\n\n\n',
+  extensions: [basicSetup, python(), oneDarkTheme],
+  parent: document.getElementsByClassName('editor1').item(0),
+});
 
 export let editor2 = new EditorView({
-    doc: `def caesar_cipher_encrypt(plaintext, shift):
+  doc: `def caesar_cipher_encrypt(plaintext, shift):
     # Your code here
     pass
 
@@ -18,6 +18,6 @@ plaintext = "HELLO"
 shift = 3
 result = caesar_cipher_encrypt(plaintext, shift)
 print(result)`,
-    extensions: [basicSetup, python(), oneDarkTheme],
-    parent: document.getElementsByClassName("editor2").item(0)
-})
+  extensions: [basicSetup, python(), oneDarkTheme],
+  parent: document.getElementsByClassName('editor2').item(0),
+});
