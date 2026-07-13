171dd390034d91a6a8147a4eeb93cdcd495e1e86
Ege Uysal <hi@egeuysal.com>
2026-07-12 19:07:42 -0500

    fix(catalog): preserve course navigation

--- Javascript/header.js
+++ Javascript/header.js
@@ -231,10 +231,6 @@ function ensureProgressScript(basePath) {
   script.id = SHARED_PROGRESS_SCRIPT_ID;
   script.src = joinBasePath(basePath, 'Javascript/progress.js');
   document.head.appendChild(script);
-
-  const continuationScript = document.createElement('script');
-  continuationScript.src = joinBasePath(basePath, 'Javascript/pathContinuation.js');
-  document.head.appendChild(continuationScript);
 }
 
 function renderHeader() {
